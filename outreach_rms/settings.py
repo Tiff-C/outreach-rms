d5f0a41bb50c3981239d0494e28d8f22d6c7d37c
Tiff-C <deguntiffany@gmail.com>
2021-11-03 15:23:47 +0000

    Fix bug where static files arent being collected #5

--- outreach_rms/settings.py
+++ outreach_rms/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [
-    'outreach-rms.herokuapp.com'
+    'outreach-rms.herokuapp.com',
+    'localhost',
 ]
 
 
@@ -152,7 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
