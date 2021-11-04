f9c5f25b834710cac96ffbc358aaa2d2f40b3142
Tiff-C <deguntiffany@gmail.com>
2021-11-04 15:15:04 +0000

    Change sqlite3 to postgress db for deployment

--- outreach_rms/settings.py
+++ outreach_rms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import mimetypes
+import dj_database_url
 import django_heroku
 if os.path.exists("env.py"):
     import env  # noqa
@@ -110,13 +111,16 @@ WSGI_APPLICATION = 'outreach_rms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
