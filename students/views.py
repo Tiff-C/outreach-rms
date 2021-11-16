9448906dabcb3603ba7ffe299bb8caf95022c5e9
Tiff-C <deguntiffany@gmail.com>
2021-11-16 23:31:18 +0000

    Fix error in StudentForm import

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Student
-from .forms import SchoolForm
+from .forms import StudentForm
 # Create your views here.
 
 
