737b337fcf1dccc2c76c97f4ddd106f61aa9fcc9
Tiff-C <deguntiffany@gmail.com>
2021-11-17 13:25:39 +0000

    Add CourseClass urls, views and templates #13

--- courses/urls.py
+++ courses/urls.py
@@ -6,8 +6,17 @@ from . import views
 
 urlpatterns = [
     path('', views.all_courses, name='courses'),
+    path('classes/', views.all_classes, name='classes'),
     path('add_course/', views.add_course, name='add_course'),
+    path('add_class/', views.add_class, name='add_class'),
     path('<course_id>', views.course_details, name='course_details'),
+    path('classes/<class_id>', views.class_details, name='class_details'),
     path('edit_course/<course_id>', views.edit_course, name='edit_course'),
-    path('delete_course/<course_id>', views.delete_course, name='delete_course'),
+    path('edit_class/<class_id>', views.edit_class, name='edit_class'),
+    path(
+        'delete_course/<course_id>',
+        views.delete_course,
+        name='delete_course'
+    ),
+    path('delete_class/<class_id>', views.delete_class, name='delete_class'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 """ Import required modules for use in courses app views """
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Course
+from .models import Course, CourseClass
 from .forms import CourseForm, CourseClassForm
 
 # Create your views here.
@@ -18,6 +18,16 @@ def all_courses(request):
 
 
 @login_required
+def all_classes(request):
+    """ A view to show all classes """
+    classes = CourseClass.objects.all()
+    context = {
+        'classes': classes
+    }
+    return render(request, 'courses/classes.html', context)
+
+
+@login_required
 def add_course(request):
     """ A view to add a new course """
     if request.method == 'POST':
@@ -31,6 +41,22 @@ def add_course(request):
         'form': form
     }
     return render(request, 'courses/add_course.html', context)
+
+
+@login_required
+def add_class(request):
+    """ A view to add a new class """
+    if request.method == 'POST':
+        form = CourseClassForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('classes')
+    form = CourseClassForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'courses/add_class.html', context)
 
 
 @login_required
@@ -52,10 +78,35 @@ def edit_course(request, course_id):
 
 
 @login_required
+def edit_class(request, class_id):
+    """ A view to edit a class """
+    course_class = get_object_or_404(CourseClass, pk=class_id)
+    if request.method == 'POST':
+        form = CourseClassForm(request.POST, instance=course_class)
+        if form.is_valid():
+            form.save()
+
+            return redirect('classes')
+    form = CourseClassForm(instance=course_class)
+    context = {
+        'form': form,
+        'course_class': course_class
+    }
+    return render(request, 'courses/edit_class.html', context)
+
+
+@login_required
 def delete_course(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     course.delete()
     return redirect('courses')
+
+
+@login_required
+def delete_class(request, class_id):
+    course_class = get_object_or_404(CourseClass, pk=class_id)
+    course_class.delete()
+    return redirect('classes')
 
 
 @login_required
@@ -68,3 +119,15 @@ def course_details(request, course_id):
     }
 
     return render(request, 'courses/course_details.html', context)
+
+
+@login_required
+def class_details(request, class_id):
+    """ A view to show class details """
+    course_class = get_object_or_404(CourseClass, pk=class_id)
+
+    context = {
+        'course_class': course_class,
+    }
+
+    return render(request, 'courses/class_details.html', context)
