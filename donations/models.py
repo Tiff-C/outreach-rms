1eaada697d4b944fd36d404c394af5a4acaf2fa2
Tiff-C <deguntiffany@gmail.com>
2021-11-25 16:31:05 +0000

    Add forms to view & template, update model

--- donations/models.py
+++ donations/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 class Donation(models.Model):
     """ A class to define the donations model """
-    name = models.CharField("Full Name", max_length=70, blank=False)
+    name = models.CharField(
+        "Full Name (Optional)",
+        default="Anonymous",
+        max_length=70,
+        blank=True)
     email = models.EmailField("Email Address", max_length=320, blank=False)
     date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    donation_amount = models.DecimalField(decimal_places=2)
 
     def __str__(self):
-        return f'{self.id}-{self.date}-{self.name}'
+        return f"{self.name} donated {self.donation_amount}"
--- donations/views.py
+++ donations/views.py
@@ -1,8 +1,52 @@
-""" Imports for home app views """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+from .forms import DonationForm, PaymentForm
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET
 
 
 def donations(request):
     """ A view to return the donations page """
+    if request.method == 'POST':
+        donation_form = DonationForm(request.POST)
+        payment_form = PaymentForm(request.POST)
+        if donation_form.is_valid() and payment_form.is_valid():
+            donation = donation_form.save()
+            total = 0
 
-    return render(request, 'donations/donations.html')
+            total += donation.amount
+
+            try:
+                customer = stripe.Charge.create(
+                    amount=(total * 100),
+                    currency="GBP",
+                    description=donation.name,
+                    card=payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your Card Was Declined")
+
+            if customer.paid:
+                messages.success(
+                    request,
+                    "You have successfully paid, thank you for your donation."
+                )
+                return redirect('dontations')
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(
+                request, "We were unable to take payment with that card")
+    else:
+        donation_form = DonationForm()
+        payment_form = PaymentForm()
+        context = {
+            'donation_form': donation_form,
+            'payment_form': payment_form,
+            'publishable': settings.STRIPE_PUBLISHABLE
+        }
+
+    return render(request, 'donations/donations.html', context)
