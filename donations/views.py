1d63814a38323c624367de853c756748f929a1e0
Tiff-C <deguntiffany@gmail.com>
2022-03-06 22:32:29 +0000

    Get card element rendering #22

--- donations/views.py
+++ donations/views.py
@@ -7,88 +7,46 @@ import stripe
 from .forms import DonationForm # PaymentForm
 import json
 
-@require_POST
-def create_payment_intent(request):
-
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.data)
-            # Create a PaymentIntent with the order amount and currency
-            intent = stripe.PaymentIntent.create(
-                amount=total,
-                currency='gbp',
-                automatic_payment_methods={
-                    'enabled': True,
-                },
-            )
-            return JsonResponse({
-                'clientSecret': intent['client_secret']
-            })
-        except Exception as e:
-            return JsonResponse(error=str(e)), 403
-
-def donations(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
-
-    donation_form = DonationForm()
-
-    if request.method == 'POST':
-
-        donation_form = DonationForm(request.POST)
-
-        if donation_form.is_valid():
-            donation = donation_form.save(commit=False)
-            total = 0
-            total += donation.donation_amount
-            donation_form.save()
-
-    template = 'donations/donations.html'
-    context = {
-        'donation_form': donation_form,
-        # 'payment_form': payment_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
-
-    return render(request, template, context)
-
-
-
-# copied and amended from Boutique Ado walkthrough.
-# (https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/b5e178737596a1a1cf5be50345dc770b119918fd/checkout/views.py)
-
-
 # @require_POST
-# def cache_checkout_data(request):
-#     try:
-#         pid = request.POST.get('client_secret').split('_secret')[0]
-#         stripe.api_key = settings.STRIPE_SECRET_KEY
-#         stripe.PaymentIntent.modify(pid, metadata={
-#             'save_info': request.POST.get('save_info'),
-#             'username': request.user,
-#         })
-#         return HttpResponse(status=200)
-#     except Exception as e:
-#         messages.error(request, 'Sorry, your payment cannot be \
-#             processed right now. Please try again later.')
-#         return HttpResponse(content=e, status=400)
+# def create_payment_intent(request):
 
+#     if request.method == 'POST':
+#         try:
+#             data = json.loads(request.data)
+#             # Create a PaymentIntent with the order amount and currency
+#             intent = stripe.PaymentIntent.create(
+#                 amount=total,
+#                 currency='gbp',
+#                 automatic_payment_methods={
+#                     'enabled': True,
+#                 },
+#             )
+#             return JsonResponse({
+#                 'clientSecret': intent['client_secret']
+#             })
+#         except Exception as e:
+#             return JsonResponse(error=str(e)), 403
 
 # def donations(request):
 #     stripe_public_key = settings.STRIPE_PUBLIC_KEY
 #     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-#     donation_form = DonationForm(request.POST)
-#     # payment_form = PaymentForm(request.POST)
+#     donation_form = DonationForm()
 
 #     if request.method == 'POST':
 
-#         if donation_form.is_valid():
-#             donation = donation_form
-#             total = 0
-#             total += donation.amount
+#         donation_form = DonationForm(request.POST)
 
+#         if donation_form.is_valid():
+#             donation = donation_form.save(commit=False)
+#             # Code from boutique ado
+#             pid = request.POST.get('client_secret').split('_secret')[0]
+#             donation.stripe_pid = pid
+#             # End of copied code
+#             total = 0
+#             total += donation.donation_amount
+#             donation_form.save()
+#             # Code from boutique ado
 #             request.session['save_info'] = 'save-info' in request.POST
 #             return redirect(reverse(
 #                 'checkout_success', args=[donation.name]
@@ -98,10 +56,9 @@ def donations(request):
 #                 Please double check your information.')
 
 #         order_total = total
-#         stripe_total = round(order_total * 100)
 #         stripe.api_key = stripe_secret_key
 #         intent = stripe.PaymentIntent.create(
-#             amount=stripe_total,
+#             amount=order_total,
 #             currency=settings.STRIPE_CURRENCY,
 #         )
 
@@ -109,36 +66,105 @@ def donations(request):
 #     if not stripe_public_key:
 #         messages.warning(request, 'Stripe public key is missing. \
 #             Did you forget to set it in your environment?')
+#             # End of copied code
 
 #     template = 'donations/donations.html'
 #     context = {
 #         'donation_form': donation_form,
 #         # 'payment_form': payment_form,
 #         'stripe_public_key': stripe_public_key,
+#         'client_secret': intent.client_secret,
 #     }
 
 #     return render(request, template, context)
 
 
-# def checkout_success(request, order_number):
-#     """
-#     Handle successful checkouts
-#     """
-#     save_info = request.session.get('save_info')
-#     order = get_object_or_404(Order, order_number=order_number)
-#     messages.success(request, f'Order successfully processed! \
-#         Your order number is {order_number}. A confirmation \
-#         email will be sent to {order.email}.')
 
-#     if 'bag' in request.session:
-#         del request.session['bag']
+# copied and amended from Boutique Ado walkthrough.
+# (https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/b5e178737596a1a1cf5be50345dc770b119918fd/checkout/views.py)
 
-#     template = 'checkout/checkout_success.html'
-#     context = {
-#         'order': order,
-#     }
 
-#     return render(request, template, context)
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
+
+def donations(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    donation_form = DonationForm(request.POST)
+    # payment_form = PaymentForm(request.POST)
+
+    if request.method == 'POST':
+
+        if donation_form.is_valid():
+            donation = donation_form
+            total = 0
+            total += donation.amount
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse(
+                'checkout_success', args=[donation.name]
+            ))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+        order_total = total
+        stripe_total = round(order_total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'donations/donations.html'
+    context = {
+        'donation_form': donation_form,
+        # 'payment_form': payment_form,
+        'stripe_public_key': stripe_public_key,
+        # 'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
 
 # def donations(request):
 #     """ A view to return the donations page """
--- static/js/stripe-element.js
+++ static/js/stripe-element.js
@@ -1,117 +1,237 @@
-// Copied from https://stripe.com/docs/payments/quickstart and amended to suit my project
+// Copied from boutique ado and amended to suit my project
+// https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/b5e178737596a1a1cf5be50345dc770b119918fd/checkout/static/checkout/js/stripe_elements.js
 
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-
-// The items the customer wants to buy
-const items = [{ id: "xl-tshirt" }];
-      
-let elements;
-      
-initialize();
-checkStatus();
-
-document
-    .querySelector("#payment-form")
-    .addEventListener("submit", handleSubmit);
-
-// Fetches a payment intent and captures the client secret
-async function initialize() {
-    const response = await fetch("/create-payment-intent", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ items }),
-    });
-    const { clientSecret } = await response.json();
-
-    const appearance = {
-    theme: 'stripe',
-    };
-    elements = stripe.elements({ appearance, clientSecret });
-
-    const paymentElement = elements.create("payment");
-    paymentElement.mount("#payment-element");
-}
-
-async function handleSubmit(e) {
-    e.preventDefault();
-    setLoading(true);
-
-    const { error } = await stripe.confirmPayment({
-    elements,
-    confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: "http://localhost:4242/checkout.html",
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
-    });
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style:style},);
+card.mount('#card-element');
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
-    showMessage(error.message);
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
     } else {
-    showMessage("An unexpected error occured.");
+        errorDiv.textContent = '';
     }
+});
 
-    setLoading(false);
-}
+// Handle form submit
+var form = document.getElementById('payment-form');
 
-// Fetches the payment intent status after payment submission
-async function checkStatus() {
-    const clientSecret = new URLSearchParams(window.location.search).get(
-    "payment_intent_client_secret"
-    );
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
-    if (!clientSecret) {
-    return;
-    }
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/donations/create_payment_intent/';
 
-    const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.name.value),
+                    email: $.trim(form.email.value),
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
+        //location.reload();
+        console.log('oh no, I failed again')
+    })
+});
 
-    switch (paymentIntent.status) {
-    case "succeeded":
-        showMessage("Payment succeeded!");
-        break;
-    case "processing":
-        showMessage("Your payment is processing.");
-        break;
-    case "requires_payment_method":
-        showMessage("Your payment was not successful, please try again.");
-        break;
-    default:
-        showMessage("Something went wrong.");
-        break;
-    }
-}
 
-// ------- UI helpers -------
 
-function showMessage(messageText) {
-    const messageContainer = document.querySelector("#payment-message");
 
-    messageContainer.classList.remove("hidden");
-    messageContainer.textContent = messageText;
+// // Copied from https://stripe.com/docs/payments/quickstart and amended to suit my project
 
-    setTimeout(function () {
-    messageContainer.classList.add("hidden");
-    messageText.textContent = "";
-    }, 4000);
-}
 
-// Show a spinner on payment submission
-function setLoading(isLoading) {
-    if (isLoading) {
-    // Disable the button and show a spinner
-    document.querySelector("#submit").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
-    } else {
-    document.querySelector("#submit").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
-    }
-}
+// var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+// var stripe = Stripe(stripe_public_key);
+
+// // The items the customer wants to buy
+// const items = [{ id: "xl-tshirt" }];
+      
+// let elements;
+      
+// initialize();
+// checkStatus();
+
+// document
+//     .querySelector("#payment-form")
+//     .addEventListener("submit", handleSubmit);
+
+// // Fetches a payment intent and captures the client secret
+// async function initialize() {
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//     const response = await fetch("/create-payment-intent", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ items }),
+//     });
+//     const { clientSecret } = await response.json();
+
+//     const appearance = {
+//     theme: 'stripe',
+//     };
+//     elements = stripe.elements({ appearance, clientSecret });
+
+//     const paymentElement = elements.create("payment");
+//     paymentElement.mount("#payment-element");
+// }
+
+// async function handleSubmit(e) {
+//     e.preventDefault();
+//     setLoading(true);
+
+//     const { error } = await stripe.confirmPayment({
+//     elements,
+//     confirmParams: {
+//         // Make sure to change this to your payment completion page
+//         return_url: "http://localhost:4242/checkout.html",
+//     },
+//     });
+
+//     // This point will only be reached if there is an immediate error when
+//     // confirming the payment. Otherwise, your customer will be redirected to
+//     // your `return_url`. For some payment methods like iDEAL, your customer will
+//     // be redirected to an intermediate site first to authorize the payment, then
+//     // redirected to the `return_url`.
+//     if (error.type === "card_error" || error.type === "validation_error") {
+//     showMessage(error.message);
+//     } else {
+//     showMessage("An unexpected error occured.");
+//     }
+
+//     setLoading(false);
+// }
+
+// // Fetches the payment intent status after payment submission
+// async function checkStatus() {
+//     const clientSecret = new URLSearchParams(window.location.search).get(
+//     "payment_intent_client_secret"
+//     );
+
+//     if (!clientSecret) {
+//     return;
+//     }
+
+//     const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+
+//     switch (paymentIntent.status) {
+//     case "succeeded":
+//         showMessage("Payment succeeded!");
+//         break;
+//     case "processing":
+//         showMessage("Your payment is processing.");
+//         break;
+//     case "requires_payment_method":
+//         showMessage("Your payment was not successful, please try again.");
+//         break;
+//     default:
+//         showMessage("Something went wrong.");
+//         break;
+//     }
+// }
+
+// // ------- UI helpers -------
+
+// function showMessage(messageText) {
+//     const messageContainer = document.querySelector("#payment-message");
+
+//     messageContainer.classList.remove("hidden");
+//     messageContainer.textContent = messageText;
+
+//     setTimeout(function () {
+//     messageContainer.classList.add("hidden");
+//     messageText.textContent = "";
+//     }, 4000);
+// }
+
+// // Show a spinner on payment submission
+// function setLoading(isLoading) {
+//     if (isLoading) {
+//     // Disable the button and show a spinner
+//     document.querySelector("#submit").disabled = true;
+//     document.querySelector("#spinner").classList.remove("hidden");
+//     document.querySelector("#button-text").classList.add("hidden");
+//     } else {
+//     document.querySelector("#submit").disabled = false;
+//     document.querySelector("#spinner").classList.add("hidden");
+//     document.querySelector("#button-text").classList.remove("hidden");
+//     }
+// }
